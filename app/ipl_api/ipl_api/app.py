ff4e00040faa76327b08517057fbb6c67c25a00d
Izukend <98744675+Izukend@users.noreply.github.com>
2023-06-14 16:17:34 +0200

    Update app.py

--- app/ipl_api/ipl_api/app.py
+++ app/ipl_api/ipl_api/app.py
@@ -4,23 +4,26 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import Table, Column, Integer, String
 import json
-from ipl_api.models import Cpl, Base, Regions
+from ipl_api.models import Cpl, Base, Serveurs
 
-
-
+# Définition du token d'authentification
 TOKEN = 'testn'
 
 class TokenAuthMiddleware:
     def process_request(self, req, resp):
+        # Vérifier si le token d'authentification est présent dans l'en-tête
         token = req.get_header('Authorization')
         if token and token == f'Bearer {TOKEN}':
-            req.context['user'] = {'username': 'izukend'}
+            # Authentification réussie, ajouter l'utilisateur au contexte de la requête
+            req.context['user'] = {'username': 'tom'}
         else:
+            # Authentification échouée, renvoyer une erreur 401 Unauthorized
             raise falcon.HTTPUnauthorized('Authentication required', 'Token authentication failed')
-        
+
+# Création de l'application Falcon avec le middleware d'authentification
 app = application = falcon.App(middleware=[TokenAuthMiddleware()])
 
-# Créer le moteur SQLAlchemy et l'ajout des tables
+# Création du moteur SQLAlchemy et ajout des tables
 engine = create_engine("mysql+mysqlconnector://root:password@mysql/falcon_bdd")
 metadata = MetaData(bind=engine)
 Session = sessionmaker(bind=engine)
@@ -28,22 +31,26 @@ session = Session()
 
 Base.metadata.create_all(engine)
 
-# Définir la structure de la table
+# Définition de la structure des tables
 cpl = Cpl.__table__
+srv = Serveurs.__table__
 
-class API:
+class APICPL:
     def __init__(self, table):
         self.post_data = []
         self.table = table
 
     def on_post(self, req, resp):
+        # Récupérer l'utilisateur authentifié à partir du contexte de la requête
         user = req.context['user']
         resp.body = f'Authenticated user: {user["username"]}'
         resp.status = falcon.HTTP_200
         try:
+            # Récupérer les données JSON de la requête
             data = json.loads(req.stream.read())
             data_add = Cpl(uid=data['uid'], name=data['name'])
 
+            # Ajouter les données à la session SQLAlchemy et les sauvegarder dans la base de données
             session.add(data_add)
             session.commit()
 
@@ -51,27 +58,70 @@ class API:
             resp.body = 'Data successfully received and saved.'
             
         except SQLAlchemyError as e:
+            # En cas d'erreur SQLAlchemy, effectuer un rollback de la session
             session.rollback()
 
             resp.status = falcon.HTTP_500
             resp.body = f'Error occurred while saving data: {str(e)}'
         except Exception as e:
-
+            # En cas d'erreur non SQLAlchemy, renvoyer une erreur 500 Internal Server Error
             resp.status = falcon.HTTP_500
             resp.body = f'Error occurred: {str(e)}'
 
-
     def on_get(self, req, resp):
+        # Récupérer l'utilisateur authentifié à partir du contexte de la requête
         user = req.context['user']
         resp.body = f'Authenticated user: {user["username"]}'
         resp.status = falcon.HTTP_200
 
+        # Exécuter une requête SQLAlchemy pour récupérer les données de la table correspondante
         query = self.table.select()
         result = session.execute(query)
 
+        # Convertir les lignes de résultat en une liste de dictionnaires
         rows = [dict(row) for row in result]
 
+        # Renvoyer les données sous forme de JSON dans le corps de la réponse
         resp.body = json.dumps(rows)
         resp.status = falcon.HTTP_200
 
-app.add_route('/', API(cpl))
+class APIServeurs:
+    def __init__(self, table):
+        self.post_data = []
+        self.table = table
+
+    def on_post(self, req, resp):
+        try:
+            # Récupérer la valeur de l'en-tête X-Forwarded-For
+            forwarded_host = req.access_route
+            
+            if forwarded_host:
+            # La valeur de X-Forwarded-For est présente, extraire la première adresse IP
+                ip_addresses = forwarded_host.split(',')
+                client_ip = ip_addresses[0].strip()
+            else:
+            # La valeur de X-Forwarded-For est absente, utiliser l'adresse IP directe du client
+                client_ip = req.remote_addr
+
+            # Ajouter l'adresse IP du client à la table correspondante de la base de données
+            public_ip_obj = Serveurs(public_ip=client_ip)
+            session.add(public_ip_obj)
+            session.commit()
+
+            resp.status = falcon.HTTP_200
+            resp.body = 'Data successfully received and saved.'
+            
+        except SQLAlchemyError as e:
+            # En cas d'erreur SQLAlchemy, effectuer un rollback de la session
+            session.rollback()
+
+            resp.status = falcon.HTTP_500
+            resp.body = f'Error occurred while saving data: {str(e)}'
+        except Exception as e:
+            # En cas d'erreur non SQLAlchemy, renvoyer une erreur 500 Internal Server Error
+            resp.status = falcon.HTTP_500
+            resp.body = f'Error occurred: {str(e)}'
+
+# Ajout des routes à l'application Falcon en associant les classes de ressources aux tables de base de données correspondantes
+app.add_route('/srv', APIServeurs(srv))
+app.add_route('/', APICPL(cpl))
