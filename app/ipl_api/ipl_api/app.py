2d014470f3d4e8909b34829aa05329ee4f8cf9ca
Izukend <98744675+Izukend@users.noreply.github.com>
2023-06-15 14:45:08 +0200

    Update app.py

--- app/ipl_api/ipl_api/app.py
+++ app/ipl_api/ipl_api/app.py
@@ -6,31 +6,31 @@ from ipl_api.cpl_data import Cpl_data, cpl
 from ipl_api.regions_data import Regions_data, regions
 from ipl_api.models import Base
 
-# Définition du token d'authentification
-TOKEN = 'githubtoken'
+# Definition of the authentication token
+TOKEN = 'testn'
 
 class TokenAuthMiddleware:
     def process_request(self, req, resp):
-        # Vérification du token d'authentification
+        # Check the authentication token
         token = req.get_header('Authorization')
         if token and token == f'Bearer {TOKEN}':
-            req.context['user'] = {'username': 'izukend'}  # Utilisateur authentifié
+            req.context['user'] = {'username': 'tom'}  # Authenticated user
         else:
             raise falcon.HTTPUnauthorized('Authentication required', 'Token authentication failed')
 
-# Création de l'application Falcon avec le middleware d'authentification
+# Create Falcon application with the authentication middleware
 app = application = falcon.App(middleware=[TokenAuthMiddleware()])
 
-# Configuration de la connexion à la base de données
+# Configure the database connection
 engine = create_engine("mysql+mysqlconnector://root:password@mysql/falcon_bdd")
 metadata = MetaData(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Création des tables de la base de données si elles n'existent pas
+# Create database tables if they don't exist
 Base.metadata.create_all(engine)
 
-# Ajout des routes pour les ressources API
+# Add routes for the API resources
 app.add_route('/servers', Servers_data(srv))
 app.add_route('/regions', Regions_data(regions))
 app.add_route('/cpl', Cpl_data(cpl))
