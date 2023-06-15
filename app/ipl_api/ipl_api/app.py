997b6aa7190d98cb03cfedce0df96148709a2480
Izukend <98744675+Izukend@users.noreply.github.com>
2023-06-15 11:16:43 +0200

    Update app.py

--- app/ipl_api/ipl_api/app.py
+++ app/ipl_api/ipl_api/app.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from ipl_api.servers_data import Servers_data, srv
 from ipl_api.cpl_data import Cpl_data, cpl
+from ipl_api.regions_data import Regions_data, regions
 from ipl_api.models import Base
 # Définition du token d'authentification
 TOKEN = 'testn'
@@ -11,13 +12,9 @@ class TokenAuthMiddleware:
     def process_request(self, req, resp):
 
         token = req.get_header('Authorization')
-
         if token and token == f'Bearer {TOKEN}':
-
             req.context['user'] = {'username': 'tom'}
-
         else:
-
             raise falcon.HTTPUnauthorized('Authentication required', 'Token authentication failed')
 
 
@@ -29,5 +26,6 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base.metadata.create_all(engine)
 
-app.add_route('/srv', Servers_data(srv))
-app.add_route('/', Cpl_data(cpl))
+app.add_route('/servers', Servers_data(srv))
+app.add_route('/regions', Regions_data(regions))
+app.add_route('/cpl', Cpl_data(cpl))
