236146b23f298973000a704a1f6ab8a3d3249100
Izukend <98744675+Izukend@users.noreply.github.com>
2023-06-15 17:05:57 +0200

    Update cpl_data.py

--- app/ipl_api/ipl_api/cpl_data.py
+++ app/ipl_api/ipl_api/cpl_data.py
@@ -12,16 +12,13 @@ class Cpl_data:
 
     def on_post(self, req, resp):
         from ipl_api.app import session
-        # Retrieve the authenticated user from the request context
         user = req.context['user']
         resp.body = f'Authenticated user: {user["username"]}'
         resp.status = falcon.HTTP_200
         try:
-            # Retrieve JSON data from the request
             data = json.loads(req.stream.read())
-            data_add = Cpl(uid=data['uid'], name=data['name'])
+            data_add = Cpl(id=data['id'], uid=data['uid'], name=data['name'])
 
-            # Add the data to the SQLAlchemy session and save it to the database
             session.add(data_add)
             session.commit()
 
@@ -29,30 +26,25 @@ class Cpl_data:
             resp.body = 'Data successfully received and saved.'
 
         except SQLAlchemyError as e:
-            # In case of a SQLAlchemy error, perform a session rollback
             session.rollback()
 
             resp.status = falcon.HTTP_500
             resp.body = f'Error occurred while saving data: {str(e)}'
         except Exception as e:
-            # In case of a non-SQLAlchemy error, return a 500 Internal Server Error
             resp.status = falcon.HTTP_500
             resp.body = f'Error occurred: {str(e)}'
 
     def on_get(self, req, resp):
         from ipl_api.app import session
-        # Retrieve the authenticated user from the request context
+
         user = req.context['user']
         resp.body = f'Authenticated user: {user["username"]}'
         resp.status = falcon.HTTP_200
 
-        # Execute a SQLAlchemy query to retrieve data from the corresponding table
         query = self.table.select()
         result = session.execute(query)
 
-        # Convert the result rows into a list of dictionaries
         rows = [dict(row) for row in result]
 
-        # Return the data as JSON in the response body
         resp.body = json.dumps(rows)
         resp.status = falcon.HTTP_200
