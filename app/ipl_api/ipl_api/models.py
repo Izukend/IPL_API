8f56badea1b5a69f7ab3eb337a24d15ca4ae8955
Izukend <98744675+Izukend@users.noreply.github.com>
2023-06-15 16:27:51 +0200

    Update models.py

--- app/ipl_api/ipl_api/models.py
+++ app/ipl_api/ipl_api/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -17,19 +16,19 @@ class Regions(Base):
 
 class Servers(Base):
     __tablename__ = 'servers'
-
+    
     id = Column(Integer, primary_key=True)
     id_region = Column(Integer, ForeignKey("regions.id"))
     name = Column(String(50))
     private_ip = Column(String(13))
     public_ip = Column(String(13))
-    last_seen = Column(DateTime(timezone=True), server_default=func.now())
+    last_seen = Column(DateTime(timezone=True))
     version_sw = Column(String(7))
 
     region = relationship("Regions", back_populates="servers")
     cpl = relationship("Cpl", back_populates="servers")
     relations = relationship("Relations", back_populates="servers")
-
+    
     def __repr__(self):
         return "<Servers(name='%s', private_ip='%s', id_region='%s')>" % (self.name, self.private_ip, self.id_region)
 
