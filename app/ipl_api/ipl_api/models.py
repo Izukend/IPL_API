c37cf3185be4e902acf55da0edc80250d1529b08
Izukend <98744675+Izukend@users.noreply.github.com>
2023-06-15 10:32:37 +0200

    Update models.py

--- app/ipl_api/ipl_api/models.py
+++ app/ipl_api/ipl_api/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -10,51 +10,48 @@ class Regions(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(33))
-    serveurs = relationship("Serveurs", back_populates="region")
+    servers = relationship("Servers", back_populates="region")
 
     def __repr__(self):
         return "<Regions(name='%s')>" % (self.name)
 
-class Serveurs(Base):
-    __tablename__ = 'serveurs'
+class Servers(Base):
+    __tablename__ = 'servers'
 
     id = Column(Integer, primary_key=True)
     id_region = Column(Integer, ForeignKey("regions.id"))
     name = Column(String(50))
-    private_ip_ = Column(String(13))
-    public_ip_ = Column(String(13))
-    last_seen = Column(DateTime(timezone=True),server_default=func.now())
+    private_ip = Column(String(13))
+    public_ip = Column(String(13))
+    last_seen = Column(DateTime(timezone=True), server_default=func.now())
     version_sw = Column(String(7))
 
-    region = relationship("Regions", back_populates="serveurs")
-    cpl = relationship("Cpl", back_populates="serveurs")
-    relations = relationship("Relations", back_populates="serveurs")
+    region = relationship("Regions", back_populates="servers")
+    cpl = relationship("Cpl", back_populates="servers")
+    relations = relationship("Relations", back_populates="servers")
 
     def __repr__(self):
-        return "<Serveurs(name='%s', ip_address='%s', id_region='%s')>" % (self.name, self.ip_address, self.id_region)
-
-
+        return "<Servers(name='%s', private_ip='%s', id_region='%s')>" % (self.name, self.private_ip, self.id_region)
 
 class Cpl(Base):
     __tablename__ = 'cpl'
     uid = Column(String(50), primary_key=True)
     name = Column(String(30))
-    id_serveurs = Column(Integer, ForeignKey("serveurs.id"))
-    serveurs = relationship("Serveurs", back_populates="cpl")
+    id_servers = Column(Integer, ForeignKey("servers.id"))
+    servers = relationship("Servers", back_populates="cpl")
     relations = relationship("Relations", back_populates="cpl")
 
     def __repr__(self):
-        return "<Cpl(name='%s', id_serveur='%s')>" % (self.name, self.id_serveurs)
-
+        return "<Cpl(name='%s', id_serveur='%s')>" % (self.name, self.id_servers)
 
 class Relations(Base):
     __tablename__ = 'relations'
 
     id = Column(Integer, primary_key=True)
     uid_cpl = Column(String(50), ForeignKey("cpl.uid"))
-    id_serveurs = Column(Integer, ForeignKey("serveurs.id"))
-    serveurs = relationship("Serveurs", back_populates="relations")
+    id_servers = Column(Integer, ForeignKey("servers.id"))
+    servers = relationship("Servers", back_populates="relations")
     cpl = relationship("Cpl", back_populates="relations")
 
     def __repr__(self):
-        return "<Relations(uid_cpl='%s', id_serveurs='%s')>" % (self.uid_cpl, self.id_serveurs)
+        return "<Relations(uid_cpl='%s', id_servers='%s')>" % (self.uid_cpl, self.id_servers)
